f24b8d5385e41d750d0c7565f18c54047b962ebd
Ian Brigmann <ian.brigmann1@gmail.com>
2026-05-21 09:20:53 -0400

    Update skip-ci.js

--- apps/web/scripts/skip-ci.js
+++ apps/web/scripts/skip-ci.js
@@ -2,9 +2,10 @@ const { execSync } = require("child_process");
 
 const commitMessage = execSync("git log -1 --pretty=%B").toString().trim();
 
-if (commitMessage.includes("[skip ci]")) {
-  console.log("Skipping build due to [skip ci] in commit message.");
+if (commitMessage.includes("[skip vercel]")) {
+  console.log("Skipping Vercel build due to [skip vercel] in commit message.");
   process.exit(0); // this causes Vercel to skip the build
 }
 
+console.log("Build allowed.");
 process.exit(1); // continue with build
